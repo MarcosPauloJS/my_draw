b4c0f06406690c1c8109d4b664b9828e37ca0d37
MarcosPauloJS <marcospaulooj@gmail.com>
2019-11-16 22:39:12 -0300

    primeira versão funcional

--- src/index.js
+++ src/index.js
@@ -4,18 +4,19 @@ const contextScreen = screen.getContext("2d");
 const line = {
     x : 0,
     y : 0,
-    color : "rgb(0, 0, 0)"
+    color : "rgb(0, 0, 0)",
+    moving: false
 }
 
 screen.addEventListener("mousemove", (event) =>{
-    // console.log(event.x);
     drawing(event.x, event.y);
 });
 
+
+
 function drawing(coodinateX, coodinateY){
     contextScreen.beginPath();
-    contextScreen.moveTo(0,0);
-    contextScreen.lineTo(coodinateX, coodinateY);
+    contextScreen.moveTo(coodinateX- 2, coodinateY- 192);
+    contextScreen.lineTo(coodinateX, coodinateY - 190);
     contextScreen.stroke();
-
 }
