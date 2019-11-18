de1301315246829ef95a8a91e492572d14491d65
MarcosPauloJS <marcospaulooj@gmail.com>
2019-11-17 21:02:00 -0300

    funcionando com problema com chamada de eventos

--- src/index.js
+++ src/index.js
@@ -4,20 +4,28 @@ const contextScreen = screen.getContext("2d");
 const line = {
     x : 0,
     y : 0,
+    size : 3,
     color : "rgb(0, 0, 0)",
     moving: false
 }
 
-screen.addEventListener("mousemove", (event) =>{
-    drawing(event.offsetX, event.offsetY);
-});
+
+screen.addEventListener("mousemove", event =>{
+    line.x = event.offsetX; 
+    line.y = event.offsetY; 
+    drawing();
+    }, false);
 
 
 
-function drawing(coodinateX, coodinateY){
+function drawing(){
+    
+
     contextScreen.beginPath();
     // contextScreen.moveTo(0,0);
-    contextScreen.moveTo(coodinateX, coodinateY);
-    contextScreen.lineTo(coodinateX + 3, coodinateY);
+    contextScreen.moveTo(line.x, line.y);
+    contextScreen.lineTo(line.x + 3, line.y);
     contextScreen.stroke();
+    console.log("função");
+    requestAnimationFrame(drawing)
  };
