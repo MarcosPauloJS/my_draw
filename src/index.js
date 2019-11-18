b6ec383888fc43ec16cbf093b758f3db2a0f757f
MarcosPauloJS <marcospaulooj@gmail.com>
2019-11-17 22:14:06 -0300

    estilização e criação da interface de controle

--- src/index.js
+++ src/index.js
@@ -9,26 +9,36 @@ const drawController = {
     drawing: false
 }
 
+// função para comecar o desenho
 function startDraw(e) {
     drawController.drawing = true;
+    // garante que a linha não se junte com a antetior
     contextScreen.beginPath();
 };
+// finaliza o desenho da linha assim que solta o botão do mouse 
 function finishDraw(e) {
     drawController.drawing = false;
 };
 
+// função resposavel pelo desenho
 function draw(e){
     if(drawController.drawing){
-        contextScreen.lineWidth = 6;
+        contextScreen.strokeStyle = drawController.color;
+        contextScreen.lineWidth = drawController.size;
         contextScreen.lineCap = "round";
         contextScreen.lineTo(e.offsetX, e.offsetY);
         contextScreen.stroke();
     }
-
 }
 
+// para o desenho, quando o mouse saia a da area de desenho.
+ function lostFocus(){ 
+     drawController.drawing = false 
+ }
+
+// lista de eventos 
 screen.addEventListener("mousedown", startDraw, false);
 screen.addEventListener("mouseup", finishDraw, false);
 screen.addEventListener("mousemove", draw, false);
-screen.addEventListener("mouseover", () =>{ drawController.drawing = false }, false);
+screen.addEventListener("mouseover", lostFocus, false);
 
